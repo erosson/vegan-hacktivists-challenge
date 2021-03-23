c9782885d83309762ca778b29fe698365db92815
Evan Rosson <git@erosson.org>
2021-03-22 20:21:58 -0400

    include twitter bootstrap; use a shared view layout

--- resources/views/questions/list.blade.php
+++ resources/views/questions/list.blade.php
@@ -1,40 +1,30 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Ask a Vegan</title>
-    </head>
-    <body>
-      <h1><a href="/">Ask a Vegan</a></h1>
+<x-layout>
+  <form method="post" action="">
+    @csrf
 
-      <form method="post" action="">
-        @csrf
+    <textarea name="body" placeholder="TODO random placeholder">{{ old('body') }}</textarea>
+    <div><button type="submit">Ask</button></div>
 
-        <textarea name="body" placeholder="TODO random placeholder">{{ old('body') }}</textarea>
-        <div><button type="submit">Ask</button></div>
+    @if ($errors->any())
+      <ul>
+      @foreach ($errors->all() as $error)
+        <li>{{$error}}</li>
+      @endforeach
+      </ul>
+    @endif
+  </form>
 
-        @if ($errors->any())
-          <ul>
-          @foreach ($errors->all() as $error)
-            <li>{{$error}}</li>
-          @endforeach
-          </ul>
-        @endif
-      </form>
-
-      @if ($questions)
-        <table>
-          @foreach ($questions as $question)
-            <tr>
-              <td><a href="/questions/{{$question->id}}">{{$question->body}}</a></td>
-              <td>{{$question->answers_count}} answers</td>
-              <td>{{$question->created_at}}</td>
-            </tr>
-          @endforeach
-        </table>
-      @else
-        <p>No questions yet.</p>
-      @endif
-    </body>
-</html>
+  @if ($questions)
+    <table>
+      @foreach ($questions as $question)
+        <tr>
+          <td><a href="/questions/{{$question->id}}">{{$question->body}}</a></td>
+          <td>{{$question->answers_count}} answers</td>
+          <td>{{$question->created_at}}</td>
+        </tr>
+      @endforeach
+    </table>
+  @else
+    <p>No questions yet.</p>
+  @endif
+</x-layout>
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -1,38 +1,28 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Ask a Vegan</title>
-    </head>
-    <body>
-      <h1><a href="/">Ask a Vegan</a></h1>
+<x-layout>
+  <h2>{{$question->body}}</h2>
+  <i>{{$question->created_at}}</i>
 
-      <h2>{{$question->body}}</h2>
-      <i>{{$question->created_at}}</i>
+  <form method="post" action="">
+    @csrf
 
-      <form method="post" action="">
-        @csrf
+    <textarea name="body">{{ old('body') }}</textarea>
+    <div><button type="submit">Answer</button></div>
 
-        <textarea name="body">{{ old('body') }}</textarea>
-        <div><button type="submit">Answer</button></div>
-
-        @if ($errors->any())
-          <ul>
-          @foreach ($errors->all() as $error)
-            <li>{{$error}}</li>
-          @endforeach
-          </ul>
-        @endif
-      </form>
-
+    @if ($errors->any())
       <ul>
-        @foreach ($question->answers as $answer)
-          <li>
-            <p>{{$answer->body}}</p>
-            <i>{{$answer->created_at}}</i>
-          </li>
-        @endforeach
+      @foreach ($errors->all() as $error)
+        <li>{{$error}}</li>
+      @endforeach
       </ul>
-    </body>
-</html>
+    @endif
+  </form>
+
+  <ul>
+    @foreach ($question->answers as $answer)
+      <li>
+        <p>{{$answer->body}}</p>
+        <i>{{$answer->created_at}}</i>
+      </li>
+    @endforeach
+  </ul>
+</x-layout>
